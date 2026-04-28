f821ab81a8c52d7486ec75f608721384e4fd35f2
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-04-28 11:57:36 +0530

    Time: 25 ms (44.25%) | Memory: 49.2 MB (12.85%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,29 +1,20 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // HashMap will store all the prefixSum upto the index <Sum, Frequency>
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        
-        int prefixSum = 0; // Actual Sum upto the Index
-        
-        // If prefixSum - k = 0, it means the subarray starts from index 0.
-        // We add (0, 1) to handle this case naturally.
-        hm.put(0, 1); 
-
-        int noOfSubArrayPossible = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum += nums[i];               // Accumulate sum
-            
-            int remove = prefixSum - k;         // Calculate what previous sum we need to chop off
-            
-            // If HashMap contains the exact prefixSum to remove, it means we found valid subarrays
-            if(hm.containsKey(remove)){         
-                noOfSubArrayPossible += hm.get(remove);
-            }
-            
-            // Store/Update the current prefixSum frequency in the map
-            hm.put(prefixSum, hm.getOrDefault(prefixSum, 0) + 1); 
+     HashMap<Integer,Integer> map=new HashMap<>();
+     int prefixSum=0;
+     map.put(0,1);
+     int count=0;
+     for(int i=0;i<nums.length;i++){
+        prefixSum=prefixSum+nums[i];
+        int sum=prefixSum-k;
+        if(map.containsKey(sum)){
+            count=count+map.get(sum);
         }
-        return noOfSubArrayPossible;
+        
+        map.put(prefixSum,map.getOrDefault(prefixSum,0)+1);
+        
+     }
+     return count;
+
     }
 }
