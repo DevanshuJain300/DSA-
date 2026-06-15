a525165454085bf5bdf91c46642290485caeff40
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-06-15 12:48:35 +0530

    Time: 3 ms (14.66%) | Memory: 47.2 MB (36.07%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -2,43 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n=intervals.length;
+        int[][] arr=new int[n+1][2];
 
-        int n = intervals.length;
-
-        // Create new array of size n+1
-        int[][] arr = new int[n + 1][2];
-
-        // Copy old intervals
-        for (int i = 0; i < n; i++) {
-            arr[i] = intervals[i];
+        for(int i=0;i<n;i++){
+            arr[i]=intervals[i];
         }
+        arr[n]=newInterval;
 
-        // Add new interval
-        arr[n] = newInterval;
-
-        // Sort by start time
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-
-        // Merge intervals
-        List<int[]> result = new ArrayList<>();
-
-        int start = arr[0][0];
-        int end = arr[0][1];
-
-        for (int i = 1; i < arr.length; i++) {
-
-            if (arr[i][0] <= end) {
-                end = Math.max(end, arr[i][1]);
-            } else {
-                result.add(new int[]{start, end});
-                start = arr[i][0];
-                end = arr[i][1];
+        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+        ArrayList<int[]> result=new ArrayList<>();
+        int start=arr[0][0];
+        int end=arr[0][1];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i][0]<=end){
+                end=(Math.max(end,arr[i][1]));
             }
+            else{
+                result.add(new int []{start,end});
+                start=arr[i][0];
+                end=arr[i][1];
+            } 
+        }        
+            result.add(new int[]{start,end});
+            return result.toArray(new int[result.size()][]);
         }
-
-        // Add the last merged interval
-        result.add(new int[]{start, end});
-
-        return result.toArray(new int[result.size()][]);
-    }
 }
