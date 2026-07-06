7e74ad4c8c42b4916a53e2785f7a00ab38ad39b4
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-07-06 13:55:55 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (95.72%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -2,8 +2,10 @@ class Solution {
     public int longestPalindrome(String s) {
         int len=0; 
         int freq[]=new int [128];
-        for(char ch:s.toCharArray()){
-            freq[ch]++;
+        for(char ch:s.toCharArray()){  // in this case , size of array is 128 because it contain A-Z also
+            freq[ch]++;                //since it also contain A-Z, freq[ch-'a']++ is wrong
+                                       // ASCII A-Z = 65-90 , a-z = 97-122
+                                       //we can use 123,124, 125,...   but 128 is standard choice
         }
         for(int i=0;i<freq.length;i++){
             if(freq[i]%2==0){
