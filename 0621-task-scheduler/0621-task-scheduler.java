c0077c8fc2c4b3d2191763895c181d79a0292fc7
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-08-03 19:32:11 +0530

    Time: 4 ms (73.91%), Space: 48.6 MB (11.49%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,17 +1,21 @@
-// (c[25] - 1) * (n + 1) + 25 - i  is frame size
-// when inserting chars, the frame might be "burst", then tasks.length takes precedence
-// when 25 - i > n, the frame is already full at construction, the following is still valid.
-public class Solution {
+class Solution {
     public int leastInterval(char[] tasks, int n) {
+    int[] ch=new int[26];
+    for(char i:tasks){
+        ch[i-'A']++;
+    }        
+    Arrays.sort(ch);
+    
+    int maxFreq=ch[25];
+    int gaddhe=maxFreq-1;
+    int idleSlots=n*gaddhe;
 
-        int[] c = new int[26];
-        for(char t : tasks){
-            c[t - 'A']++;
-        }
-        Arrays.sort(c);
-        int i = 25;
-        while(i >= 0 && c[i] == c[25]) i--;
-
-        return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
+    for(int i=24;i>=0;i--){
+        idleSlots=idleSlots - Math.min (ch[i] , gaddhe);
+    }
+    if(idleSlots>0){
+        return tasks.length + idleSlots;
+    }
+    return tasks.length;
     }
 }
