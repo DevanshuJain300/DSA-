c2676022a540967099715129ae7ce4c8f269771c
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-08-13 19:53:51 +0530

    Time: 19 ms (49.54%), Space: 98.3 MB (5.07%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,33 +1,22 @@
 class Solution {
+    List<List<Integer>> ans=new ArrayList<>();
+    List<Integer>sublist=new ArrayList<>();
 
-    List<List<Integer>> ans = new ArrayList<>();
-
-    public List<List<Integer>> combine(int n, int k) {
-
-        backtrack(n, k, 1, new ArrayList<>());
-
-        return ans;
-    }
-
-    public void backtrack(int n, int k, int index,
-                           List<Integer> subList) {
-
-        // Base case
-        if (subList.size() == k) {
-            ans.add(new ArrayList<>(subList));
+    public void backtrack(int n, int k, int index, List<Integer> sublist){
+        if(sublist.size()==k){
+            ans.add(new ArrayList<>(sublist));
             return;
         }
+        for(int i=index;i<=n;i++){
+            sublist.add(i);
+            backtrack(n,k,i+1,sublist);
+            sublist.remove(sublist.size()-1);
 
-        for (int i = index; i <= n; i++) {
-
-            // pick
-            subList.add(i);
-
-            // recursion
-            backtrack(n, k, i + 1, subList);
-
-            // remove last
-            subList.remove(subList.size() - 1);
         }
+    }
+
+    public List<List<Integer>> combine(int n,int k){
+        backtrack(n,k,1,sublist);
+        return ans;
     }
 }
