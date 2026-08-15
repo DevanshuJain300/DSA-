f56a2e6e6f3063bb77e83fc828781feca9cc6f20
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-08-15 21:18:49 +0530

    Time: 128 ms (87.59%), Space: 43 MB (44%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,38 +1,55 @@
 class Solution {
+
     public boolean exist(char[][] board, String word) {
-        for(int i=0;i<board.length;i++){
-        for(int j=0;j<board[0].length;j++){
-            if(board[i][j]==word.charAt(0)){
-                
-                if(backtrack(board,word,i,j,0)){
-                    return true;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (board[i][j] == word.charAt(0)) {
+
+                    if (backtrack(board, word, i, j, 0)) {
+                        return true;
+                    }
                 }
             }
         }
-    }
-    return false;
-    }
 
-    public boolean backtrack(char[][] board, String word, int i, int j, int index){
-        if(index==word.length()){
-            return true;
-        }
-        if(i<0 || i>=board.length||
-           j<0 || j>=board[0].length){
-            return false;
-        }
-    if(board[i][j] != word.charAt(index)){
         return false;
     }
-    char temp=board[i][j];
-    board[i][j]= '#';
 
-    boolean found = backtrack(board, word, i+1, j, index+1) ||
-                    backtrack(board, word, i-1, j, index+1) ||
-                    backtrack(board, word, i ,  j+1, index+1) ||
-                    backtrack(board, word, i ,  j-1, index+1) ;
+    public boolean backtrack(char[][] board, String word,
+                             int i, int j, int index) {
 
-    board[i][j] = temp;
-    return found;
+        // Word found
+        if (index == word.length()) {
+            return true;
+        }
+
+        // Out of bounds
+        if (i < 0 || i >= board.length ||
+            j < 0 || j >= board[0].length) {
+            return false;
+        }
+
+        // Wrong character
+        if (board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        // Mark visited
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        // Explore 4 directions
+        boolean found =
+            backtrack(board, word, i + 1, j, index + 1) ||
+            backtrack(board, word, i - 1, j, index + 1) ||
+            backtrack(board, word, i, j + 1, index + 1) ||
+            backtrack(board, word, i, j - 1, index + 1);
+
+        // Undo / backtrack
+        board[i][j] = temp;
+
+        return found;
     }
 }
