6acf918f467c308492489395491f4e629a00250f
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-08-27 16:51:55 +0530

    Time: 4 ms (16.94%), Space: 47.1 MB (85.04%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,20 +1,62 @@
 class Solution {
 
-    HashSet<Integer> set = new HashSet<>();
-
     public boolean findTarget(TreeNode root, int k) {
 
-        if (root == null) {
-            return false;
+        Stack<TreeNode> leftStack = new Stack<>();
+        Stack<TreeNode> rightStack = new Stack<>();
+
+        TreeNode left = root;
+        TreeNode right = root;
+
+        while (true) {
+
+            // Smallest element
+            while (left != null) {
+                leftStack.push(left);
+                left = left.left;
+            }
+
+            // Largest element
+            while (right != null) {
+                rightStack.push(right);
+                right = right.right;
+            }
+
+            if (leftStack.isEmpty() || rightStack.isEmpty()) {
+                return false;
+            }
+
+            TreeNode l = leftStack.peek();
+            TreeNode r = rightStack.peek();
+
+            if (l == r) {
+                return false;
+            }
+
+            int sum = l.val + r.val;
+
+            if (sum == k) {
+                return true;
+            }
+
+            if (sum < k) {
+
+                // Move left pointer forward
+                TreeNode node = leftStack.pop();
+
+                if (node.right != null) {
+                    left = node.right;
+                }
+
+            } else {
+
+                // Move right pointer backward
+                TreeNode node = rightStack.pop();
+
+                if (node.left != null) {
+                    right = node.left;
+                }
+            }
         }
-
-        if (set.contains(k - root.val)) {
-            return true;
-        }
-
-        set.add(root.val);
-
-        return findTarget(root.left, k) ||
-               findTarget(root.right, k);
     }
 }
