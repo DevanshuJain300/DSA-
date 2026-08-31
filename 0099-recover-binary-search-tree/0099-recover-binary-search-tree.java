93fccca65e8e98291e297fc3fc821a2589334f99
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-08-31 16:52:40 +0530

    Time: 1 ms (100%), Space: 46.9 MB (12.45%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -1,67 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
 
+    TreeNode first = null;
+    TreeNode second = null;
     TreeNode prev = null;
-
-    TreeNode g1First = null;
-    TreeNode g1Second = null;
-
-    TreeNode g2First = null;
-    TreeNode g2Second = null;
-
-    int galat = 0;
 
     public void recoverTree(TreeNode root) {
 
-        check(root);
+        inorder(root);
 
-        // Only one violation
-        if (galat == 1) {
-
-            int temp = g1First.val;
-            g1First.val = g1Second.val;
-            g1Second.val = temp;
-
-        }
-        // Two violations
-        else {
-
-            int temp = g1First.val;
-            g1First.val = g2Second.val;
-            g2Second.val = temp;
-        }
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
 
-    public void check(TreeNode root) {
+    public void inorder(TreeNode root) {
 
-        if (root == null) {
+        if (root == null)
             return;
-        }
 
-        check(root.left);
+        inorder(root.left);
 
-        if (prev != null) {
+        if (prev != null && prev.val > root.val) {
 
-            if (root.val < prev.val) {
+            if (first == null)
+                first = prev;
 
-                if (galat == 0) {
-
-                    g1First = prev;
-                    g1Second = root;
-
-                    galat++;
-
-                } else {
-
-                    g2First = prev;
-                    g2Second = root;
-
-                    galat++;
-                }
-            }
+            second = root;
         }
 
         prev = root;
 
-        check(root.right);
+        inorder(root.right);
     }
 }
