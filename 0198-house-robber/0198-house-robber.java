5f9a321f16f42cd57ed7f51c6e03f8f829bf8454
Devanshu Jain <151892510+DevanshuJain300@users.noreply.github.com>
2026-09-12 17:21:19 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,36 +1,14 @@
 class Solution {
-
-    HashMap<Integer, Integer> dp = new HashMap<>();
-
     public int rob(int[] nums) {
+        int prevRob = 0;
+        int maxRob = 0;
 
-        return solve(nums, 0);
-    }
-
-    public int solve(int[] nums, int i) {
-
-        // No houses left
-        if (i >= nums.length) {
-            return 0;
+        for (int curValue : nums) {
+            int temp = Math.max(maxRob, prevRob + curValue);
+            prevRob = maxRob;
+            maxRob = temp;
         }
 
-        // Already calculated
-        if (dp.containsKey(i)) {
-            return dp.get(i);
-        }
-
-        // Option 1: Rob current house
-        int rob = nums[i] + solve(nums, i + 2);
-
-        // Option 2: Skip current house
-        int skip = solve(nums, i + 1);
-
-        // Take the better choice
-        int ans = Math.max(rob, skip);
-
-        // Store answer
-        dp.put(i, ans);
-
-        return ans;
+        return maxRob;        
     }
 }
